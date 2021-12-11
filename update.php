376f3bb844b8239b0164702e5958398172e521c6
Tawinnie <94627449+Tawinnie@users.noreply.github.com>
2021-12-11 03:44:05 +0000

    Update update.php

--- update.php
+++ update.php
@@ -1,71 +1,10 @@
 <<<<<<< HEAD
 <?php
-//editing/updating product details
-
-//connect to database
-// include "dbconnect.php";
-// $db = mysqli_connect('localhost', 'root', '', 'business');
-// session_start();//start a session
-
-// $id = $_GET['id'];//get the id through string
-// $query=mysqli_query($db,"SELECT * FROM product WHERE product_id='$id'");
-// $fetch=mysqli_fetch_array($query);//fetch the data
-
-//         //if the user clicks on the update button
-//         if(isset($_POST['update']))
-//         {
-//             $sid=$_SESSION["id"];
-//                 $productname = $_POST["productname"];
-//                 $price = $_POST["price"];
-//                 $location = $_POST["location"];
-//                 $description = $_POST["description"];
-//                 $image=$_FILES["image"];
-//                 $root_dir = "C:\\Xamppa\\htdocs\\Final Project\\pictures\\";
-//                 $upload_root_dir = "./pictures/";
-//                 $file = $_FILES["image"];//option to upload a file
-//                 $file = str_replace(' ', '_', $file);
-//                 $file_dest = $root_dir . basename($file["name"]);
-//                 $upload_file_dest = $upload_root_dir . basename($file["name"]);
-//                 $file_type = strtolower(pathinfo($file_dest, PATHINFO_EXTENSION));
-        
-//                 echo $file_dest;
-//                 //specifying the type of image files allowed
-//                 if($file_type != "jpg" && $file_type != "png" && $file_type != "jpeg")
-//                 {
-//                     echo "Sorry, only jpg, png, jpeg allowed";
-//                 }
-//                 else
-//                 {//inserting the uploaded files into the database
-//                     $upload_image = move_uploaded_file($file["tmp_name"], $file_dest);
-//                     if($upload_image)
-//                     {
-//                         $sql= mysqli_query($db,"UPDATE product SET product_name='$productname',price='$price',image='$image',location='$location',description='$description' WHERE student_id='$sid'");
-//                         $status = mysqli_query($db,$sql);
-//                         if($status)
-//                         {
-//                             header("Location: Navbar.php");
-//                         }
-//                    }
-
-//                 //update the database with the entered data
-//             // if($sql)
-//             // {
-//             //     mysqli_close($db);//close connection
-//             //     header("location:Navbar.php");//redirect to home page
-//             //     exit;
-                
-//             // }
-//         }
-//     }
-?>
-
-
-
-<?php
+ //editing product details
 include "dbconnect.php";
 session_start();
 // connect to database
-$db = mysqli_connect('localhost', 'root', '', 'business');
+$db = mysqli_connect('localhost', 'root', 'Tawinnie', 'business');
 //if the button is not clicked
   if (!isset($_POST["update"]))
   {
@@ -114,45 +53,6 @@ $db = mysqli_connect('localhost', 'root', '', 'business');
 
     
 ?>
-
-=======
-<?php
-//editing/updating product details
-
-//connect to database
-include "dbconnect.php";
-$db = mysqli_connect('localhost', 'root', '', 'business');
-session_start();//start a session
-
-$id = $_GET['id'];//get the id through string
-$query=mysqli_query($db,"SELECT * FROM product WHERE product_id='$id'");
-$fetch=mysqli_fetch_array($query);//fetch the data
-
-        //if the user clicks on the update button
-        if(isset($_POST['update']))
-        {
-            $sid=$_SESSION["id"];
-                $productname = $_POST["productname"];
-                $price = $_POST["price"];
-                $location = $_POST["location"];
-                $description = $_POST["description"];
-                $image=$_FILES["image"];
-                $image= str_replace(' ', '_', $image);
-                //update the database with the entered data
-                $sql= mysqli_query($db,"UPDATE product SET product_name='$productname',price='$price',image='$image',location='$location',description='$description' WHERE student_id='$sid'");
-            if($sql)
-            {
-                mysqli_close($db);//close connection
-                header("location:Navbar.php");//redirect to home page
-                exit;
-                
-            }else
-                {
-                echo mysqli_error();
-                }
-        }
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
