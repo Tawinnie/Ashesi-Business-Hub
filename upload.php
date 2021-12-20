ee9fcc343e4a67d2a7b4867610ab11e987c1b2ec
Tawinnie <94627449+Tawinnie@users.noreply.github.com>
2021-12-21 01:21:27 +0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,7 +1,7 @@
 <?php
-//this is theinitial github code
-include "dbconnect.php";
 session_start();
+
+include "dbconnect.php";
 // connect to database
 $db = mysqli_connect('localhost', 'root', 'Tawinnie', 'business');
 //if the button is not clicked
@@ -13,7 +13,7 @@ $db = mysqli_connect('localhost', 'root', 'Tawinnie', 'business');
     if(isset($_POST["submit"]))
     {
         //get the id of the logged in user
-        $id = $_SESSION["id"];
+        $id = $_SESSION['id'];
         //$category=$_POST['category'];
         $prodname = $_POST["productname"];
         $price = $_POST["price"];
@@ -22,7 +22,7 @@ $db = mysqli_connect('localhost', 'root', 'Tawinnie', 'business');
         $root_dir ="20.117.153.25\\Final Project\\Ashesi-Business-Hub\\pictures\\";
         $upload_root_dir = "./pictures/";
         $file = $_FILES["image"];//option to upload a file
-        $file = str_replace(' ', '_', $file);
+        $file = str_replace(' ', '_', $file);//replace spaces with underscores in a file name
         $file_dest = $root_dir . basename($file["name"]);
         $upload_file_dest = $upload_root_dir . basename($file["name"]);
         $file_type = strtolower(pathinfo($file_dest, PATHINFO_EXTENSION));
@@ -46,7 +46,9 @@ $db = mysqli_connect('localhost', 'root', 'Tawinnie', 'business');
                     VALUES('$upload_file_dest', '$prodname','$price','$location','$desc','$id')";
                     $status = mysqli_query($db,$sql);
                     if($status)
-                        header("Location: index.php");
+                    {
+                        header("Location: Navbar.php");
+                    }
                 }
 
                
